3bd060a949d9ca80cdaec34745cc7649edd4c33c
283255625@qq.com <283255625@qq.com>
2024-07-30 08:41:27 +0800

    test mysql 链接

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const { DefaultAzureCredential,ClientSecretCredential } = require("@azure/identity");
+
+const mysql = require('mysql2');
+
+
+const credential = new DefaultAzureCredential();
+var accessToken = credential.getToken('https://ossrdbms-aad.database.windows.net/.default');
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,6 +19,24 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/test', function(req, res, next) {
+  
+  
+  const connection = mysql.createConnection({
+    host: process.env.AZURE_MYSQL_HOST,
+    user: process.env.AZURE_MYSQL_USER,
+    password: accessToken.token,
+    database: process.env.AZURE_MYSQL_DATABASE,
+    port: process.env.AZURE_MYSQL_PORT,
+    ssl: process.env.AZURE_MYSQL_SSL
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database: ' + err.stack);
+      return;
+    }
+    console.log('Connected to MySQL database');
+  });
   res.send('respond with a resource new4');
 });
 
