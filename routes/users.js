872e793db82440ffae64974f697b1e99dae2549a
283255625@qq.com <283255625@qq.com>
2024-10-11 15:58:38 +0800

    new log format

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const pino = require('pino');
-const logger = pino({
-  timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
-});
 
 const { DefaultAzureCredential,ClientSecretCredential } = require("@azure/identity");
 
@@ -22,12 +18,6 @@ router.get('/', function(req, res, next) {
 router.get('/test', async (req, res, next)=>{
   const credential = new DefaultAzureCredential();
   var accessToken = await credential.getToken('https://ossrdbms-aad.database.windows.net/.default');
-  logger.info(
-    {
-      requestID: "f9ed4675f1c53513c61a3b3b4e25b4c0",
-    },
-    accessToken,
-  );
 
 
   const connection = mysql.createConnection({
@@ -39,34 +29,14 @@ router.get('/test', async (req, res, next)=>{
     ssl:{ca: fs.readFileSync('./DigiCertGlobalRootCA.crt.pem')}
   });
   
-  logger.info(
-    {
-      requestID: "f9ed4675f1c53513c61a3b3b4e25b4c0",
-    },
-    {
-      host: process.env.AZURE_MYSQL_HOST,
-      user: process.env.AZURE_MYSQL_USER,
-      password: accessToken.token,
-      database: process.env.AZURE_MYSQL_DATABASE,
-      port: process.env.AZURE_MYSQL_PORT,
-      ssl:{ca: fs.readFileSync('./DigiCertGlobalRootCA.crt.pem')}
-    }
-  );
 
- 
   let log="";
   connection.connect((err) => {
       if (err) {
         log="mysql error";
-        logger.info({
-          requestID: "f9ed4675f1c53513c61a3b3b4e25b4c0",
-        },err)
         res.send('respond with a resource new6 '+log);
       }else{
         log="mysql success";
-        logger.info({
-          requestID: "f9ed4675f1c53513c61a3b3b4e25b4c0",
-        },log)
         res.send('respond with a resource new6 '+log);
       }
   });
